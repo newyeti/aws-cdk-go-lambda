5375de04180760632df70af35fa682de540d6e77
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2024-08-22 17:03:41 -0400

    skeleton

---
+++ function/todo/handler.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func CreateTodo(c *gin.Context) {}
+
+func GetTodo(c *gin.Context) {}
+
+func UpdateTodo(c *gin.Context) {}
+
+func DeleteTodo(c *gin.Context) {}
---
+++ function/todo/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
+)
+
+var ginLambda *ginadapter.GinLambda
+
+func init() {
+	log.Printf("Gin starting")
+
+	r := gin.Default()
+	
+	r.POST("/todo", CreateTodo)
+
+	r.GET("/todo/:id", GetTodo)
+
+	r.PUT("/todo/:id", UpdateTodo)
+
+	r.DELETE("/todo/:id", DeleteTodo)
+
+	ginLambda = ginadapter.New(r)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, req)
+}
+
+func main(){
+	lambda.Start(Handler)
+}
